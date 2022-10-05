3ea9ac3ad03f7f45882a8b66f74908f3208bb397
Imonikai <108214765+Imonikai@users.noreply.github.com>
2022-10-05 13:31:32 +0900

    いいねボタンをasync/awaitを使って分かりやすく

--- static/js/good_change.js
+++ static/js/good_change.js
@@ -1,4 +1,4 @@
-function good_change( url, btnId, goodCountId )
+async function good_change( url, btnId, goodCountId )
 {
     btn = document.querySelector('#'+btnId);
     goodCount = document.querySelector('#'+goodCountId);
@@ -7,24 +7,22 @@ function good_change( url, btnId, goodCountId )
        Credential: 'sameorigin'
     };
 
-    fetch(url, option)
-    .then( response => response.json())
-    .then( data => {
-        if( data.status === "OK" )
+    response = await fetch(url, option)
+    data = await response.json()
+    if( data.status === "OK" )
+    {
+        if( btn.className == "btn btn-danger")
         {
-            if( btn.className == "btn btn-danger")
-            {
-                btn.className = "btn btn-outline-danger";
-            }
-            else
-            {
-                btn.className = "btn btn-danger";
-            }
-            goodCount.textContent = String(data.good_count);
+            btn.className = "btn btn-outline-danger";
         }
         else
         {
-            alert('いいねに失敗しました');
+            btn.className = "btn btn-danger";
         }
-    })
+        goodCount.textContent = String(data.good_count);
+    }
+    else
+    {
+        alert('いいねに失敗しました');
+    }
 }
